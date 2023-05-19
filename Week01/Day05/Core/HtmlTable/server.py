d2a6e569c7fe41a79050b563ba37f0bac986c0db
medkahla <elkahlamed@gmail.com>
2023-05-19 19:30:37 +0100

    rectif HtmlTable

--- Week01/Day05/Core/HtmlTable/server.py
+++ Week01/Day05/Core/HtmlTable/server.py
@@ -8,13 +8,13 @@ def index():
 
 @app.route('/lists')
 def render_lists():
-    student_info = [
-       {'name' : 'Michael', 'age' : 35},
-       {'name' : 'John', 'age' : 30 },
-       {'name' : 'Mark', 'age' : 25},
-       {'name' : 'KB', 'age' : 27}
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-    return render_template("index.html", students = student_info)
+    return render_template("index.html", users = users)
 
 
 
